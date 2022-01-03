b75b8c3e116e3c2b312faaa4d5d07c84e50c8e51
donngchao <1559273720@qq.com>
2022-01-03 20:51:16 +0800

    Create for1.go

---
+++ for1.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Printf("%v", sum)
+}
