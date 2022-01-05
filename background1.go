9e6508f51c241f8839554be503c9dfc3d4954f42
donngchao <1559273720@qq.com>
2022-01-05 09:49:36 +0800

    Create background1.go

---
+++ background1.go
@@ -0,0 +1,7 @@
+// Background returns a non-nil, empty Context. It is never canceled, has no
+// values, and has no deadline. It is typically used by the main function,
+// initialization, and tests, and as the top-level Context for incoming
+// requests.
+func Background() Context {
+	return background
+}
