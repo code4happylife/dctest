7950173e70a308a704fd3a267abb9012630c87fe
donngchao <1559273720@qq.com>
2022-01-12 18:12:50 +0800

    time.Time type 类型数据使用

---
+++ time/time1.go
@@ -0,0 +1,14 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var a time.Time
+	fmt.Println(a)
+}
+//0001-01-01 00:00:00 +0000 UTC
