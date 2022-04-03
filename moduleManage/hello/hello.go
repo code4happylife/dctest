dc1980823ee56c95a1f672403faedbafd153147d
donngchao <1559273720@qq.com>
2022-04-03 09:48:22 +0800

    Update hello.go

--- moduleManage/hello/hello.go
+++ moduleManage/hello/hello.go
@@ -9,3 +9,11 @@ func main(){
 	message :=greetings.Hello("Robot")
 	fmt.Println(message)
 }
+
+/*
+
+go mod edit -replace example.com/greetings=../greetings
+go mod tidy
+go: found example.com/greetings in example.com/greetings v0.0.0-00010101000000-000000000000
+
+**/
